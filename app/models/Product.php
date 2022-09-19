dad8ab1ccb1e53e7380bd567665b602a4198a6a6
Оля <71034985+dron-olya7@users.noreply.github.com>
2022-09-19 20:05:56 +0500

    Добавлены новые методы для обращения к БД

---
+++ app/models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\models;
+
+use RedBeanPHP\R;
+
+class Product extends AppModel {
+    public function getList($params, $lang) {
+        $products = R::getAll("SELECT c.id, p.* from category c
+                                    JOIN product p on c.id = p.category_id
+                                    JOIN product_description pd on p.id = pd.product_id
+                                    WHERE c.slug = ? AND pd.languahe_id = ?", [$params, $lang]);
+        return $products;
+    }
+}
